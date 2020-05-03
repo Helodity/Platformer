e6cf707f4e8f07f5c8e7cf06666dfc74ff3bd5f7
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-05-03 11:34:55 -0400

    added playerdata

---
+++ Assets/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿public static class PlayerStats
+{
+    //Coins that were collected in another level
+    static int _SecuredCoins;
+
+    //Coins that were collected in the current level and will be lost upon death
+    public static int _RoundCoins;
+
+    #region getters
+    public static int GetTotalCoins() { return _SecuredCoins + _RoundCoins; }
+    #endregion
+
+    #region coins
+
+    public static void StoreCoins(int amount)
+    {
+        amount = _RoundCoins < 0 ? _RoundCoins : amount;
+        _SecuredCoins += amount;
+        _RoundCoins -= amount;
+    }
+
+    #endregion
+}
