4b9e331ff4c949a08713a400046df834919e8111
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-05-04 16:01:23 -0400

    converted ui files into 1 file

---
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [Header("Stamina")]
+    [SerializeField] PlayerMovement _Player;
+    [SerializeField] [Range(0, 1)] float _StaminaLerpSpeed;
+    [SerializeField] Image _StaminaBar;
+
+    [Header("Coin Counter")] 
+    [SerializeField] Text _CoinText;
+
+    void FixedUpdate()
+    {
+        _StaminaBar.fillAmount = Mathf.Lerp(_StaminaBar.fillAmount, _Player.GetCurrentStamina() / _Player.GetMaxStamina(), _StaminaLerpSpeed);
+        _CoinText.text = Global.GetTotalCoins().ToString();
+    }
+}
